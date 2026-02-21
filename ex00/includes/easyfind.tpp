77436a12c8f4f5b914eb90e95b5d7e0957308529
daniel <daniel-congo2@hotmail.com>
2026-02-21 12:29:50 +0100

    working on list

--- ex00/includes/easyfind.tpp
+++ ex00/includes/easyfind.tpp
@@ -1,51 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*   easyfind.tpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/17 17:16:41 by danielafons       #+#    #+#             */
-/*   Updated: 2026/02/20 13:00:07 by daniel149af      ###   ########.fr       */
+/*   Created: 2026/02/20 12:56:27 by daniel149af       #+#    #+#             */
+/*   Updated: 2026/02/20 13:12:58 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <iostream>
-#include <vector>
-
-class OccurenceNotFound: public std::exception{
-	const char * what() const throw(){
-		return ("Error: occurence not found.");
+#include "easyfind.hpp"
+template <typename T>
+typename T::iterator easyfind(T& a, int nb)
+{
+	for (typename T::iterator it = a.begin(); it != a.end(); it++)
+	{
+		if (*it == nb)
+			return (it);
 	}
-};
+	throw OccurenceNotFound();
+	return (a.end());
+}
 
-#include "easyfind.tpp"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// GROS RACISTE DE MERDE
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,24 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 17:15:16 by danielafons       #+#    #+#             */
-/*   Updated: 2026/02/19 18:05:21 by daafonso         ###   ########.fr       */
+/*   Updated: 2026/02/20 13:54:43 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/easyfind.hpp"
 
-int main()
+void testing_vector()
 {
+	std::cout << "\n\n### TESTING VECTOR ###\n\n" << std::endl;
+
 	std::vector<int> v;
-    int ret = easyfind(v, 1);
 	v.push_back(12);
 	v.push_back(1);
-    std::cout << ret << std::endl;
-    return 0;
+	try
+	{
+		std::vector<int>::iterator it = easyfind(v, 12);
+		if (it != v.end())
+			std::cout << *it << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
 
+void testing_list()
+{
+	std::cout << "\n\n### TESTING list ###\n\n" << std::endl;
 
-// JE TE VOIS
+	std::list<int> v;
+	v.push_back(12);
+	v.push_back(1);
+	try
+	{
+		std::list<int>::iterator it = easyfind(v, 12);
+		if (it != v.end())
+			std::cout << *it << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
+int main()
+{
+	testing_vector();
+	return 0;
+}
