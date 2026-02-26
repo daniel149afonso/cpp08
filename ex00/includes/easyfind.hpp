6bfd21c527dd641c3bc91f5f60ebf3e7ff0bd178
daniel <daniel-congo2@hotmail.com>
2026-02-26 16:20:04 +0100

    finished ex00

--- ex00/includes/easyfind.hpp
+++ ex00/includes/easyfind.hpp
@@ -6,13 +6,15 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 17:16:41 by danielafons       #+#    #+#             */
-/*   Updated: 2026/02/20 13:00:07 by daniel149af      ###   ########.fr       */
+/*   Updated: 2026/02/26 14:13:23 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 
 class OccurenceNotFound: public std::exception{
 	const char * what() const throw(){
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 17:15:16 by danielafons       #+#    #+#             */
-/*   Updated: 2026/02/20 13:54:43 by daniel149af      ###   ########.fr       */
+/*   Updated: 2026/02/26 16:19:29 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,55 @@ void testing_vector()
 
 void testing_list()
 {
-	std::cout << "\n\n### TESTING list ###\n\n" << std::endl;
+	std::cout << "\n\n### TESTING LIST ###\n\n" << std::endl;
 
-	std::list<int> v;
-	v.push_back(12);
-	v.push_back(1);
+	std::list<int> lst;
+	lst.push_front(42);
+	lst.push_front(12);
 	try
 	{
-		std::list<int>::iterator it = easyfind(v, 12);
-		if (it != v.end())
+		std::list<int>::iterator it = easyfind(lst, 12);
+		if (it != lst.end())
+			std::cout << *it << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
+void testing_deque()
+{
+	std::cout << "\n\n### TESTING DEQUE ###\n\n" << std::endl;
+
+	std::deque<int> deque;
+	
+	deque.push_front(12);
+	deque.push_front(44);
+	try
+	{
+		std::deque<int>::iterator it = easyfind(deque, 12);
+		if (it != deque.end())
+			std::cout << *it << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
+void testing_not_found()
+{
+	std::cout << "\n\n### TESTING VALUE NOT FOUND ###\n\n" << std::endl;
+
+	std::deque<int> deque;
+	
+	deque.push_front(0);
+	deque.push_front(44);
+	try
+	{
+		std::deque<int>::iterator it = easyfind(deque, 12);
+		if (it != deque.end())
 			std::cout << *it << std::endl;
 	}
 	catch(const std::exception& e)
@@ -53,5 +93,8 @@ void testing_list()
 int main()
 {
 	testing_vector();
+	testing_list();
+	testing_deque();
+	testing_not_found();
 	return 0;
 }
